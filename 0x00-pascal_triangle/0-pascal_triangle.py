be3e13dae7fd0084f3499f7d986a7c3d07fd3a04
kate mwende <kmwende419@gmail.com>
2023-07-30 21:56:28 +0300

    Pascal triangle function creation

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+pascal's triangle
+"""
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers
+    """
+    if n <= 0:
+      return []
+
+    triangle = [[1]]
+
+    for x in range(1, n):
+        row = [1]
+
+        for i in range(1, x):
+            row.append(triangle[x - 1][i - 1] + triangle[x - 1][i])
+        row.append(1)
+        triangle.append(row)
+    return triangle
+    
