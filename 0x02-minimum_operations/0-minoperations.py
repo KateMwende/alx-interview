9d0e556daf17ffc1bbe1be84e60036e0b94cba17
kate mwende <kmwende419@gmail.com>
2023-08-09 14:03:05 +0300

    Finding the least number of operations needed

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Find least number of operations to achieve 'H' n number of times
+"""
+
+
+def minOperations(n):
+    """H should be printed n number of times"""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            n //= divisor
+            operations += divisor
+        else:
+            divisor += 1
+
+    return operations
